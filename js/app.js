fc9fd6183db98620efef1d7215f7b1f41d0634b3
Alex <alex.pavia@wealthcounsel.com>
2016-09-16 09:49:07 -0600

    Finish team assembling

--- js/app.js
+++ js/app.js
@@ -15,19 +15,139 @@ app.controller("mainCTRL", function($scope, $http){
         $scope.$broadcast('peopleLoaded');
     });
 
+    $scope.showTeams = false;
+    $scope.teamFocused = false;
+    $scope.focusedTeam = {};
+    $scope.teamPlaying = {};
+    $scope.readyToStart = false;
+
     $scope.hasStarted = false;
     $scope.currentQuestion = {};
-    $scope.purpleTeam = [];
 
-    $scope.start = function() {
+    $scope.reset = function() {
+        $scope.hasStarted = false;
+        $scope.currentQuestion = {};
         $scope.currentQuestionNum = 1;
-        loadQuestion($scope.currentQuestionNum);
-        $scope.hasStarted = true;
+
+        for (var i = 0; i < $scope.teamPlaying.players.length; i++) {
+            $scope.teamPlaying.players[i].going = false;
+        }
     };
 
-    $scope.nextQuestion = function(){
-        $scope.currentQuestionNum += 1;
+    $scope.$on('peopleLoaded', function(){
+        $scope.teams = [
+            $scope.purpleTeam = {
+                "name" : "purple",
+                "people" : getTeam("purple"),
+                "hasFocus" : false
+            },
+            $scope.platinumTeam = {
+                "name" : "platinum",
+                "people" : getTeam("platinum"),
+                "hasFocus" : false
+            },
+            $scope.goldTeam = {
+                "name" : "gold",
+                "people" : getTeam("gold"),
+                "hasFocus" : false
+            },
+            $scope.paisleyTeam = {
+                "name" : "paisley",
+                "people" : getTeam("paisley"),
+                "hasFocus" : false
+            },
+            $scope.titaniumTeam = {
+                "name" : "titanium",
+                "people" : getTeam("titanium"),
+                "hasFocus" : false
+            },
+            $scope.blackTeam = {
+                "name" : "black",
+                "people" : getTeam("black"),
+                "hasFocus" : false
+            },
+            $scope.psychedelicTeam = {
+                "name" : "psychedelic",
+                "people" : getTeam("psychedelic"),
+                "hasFocus" : false
+            },
+            $scope.greenTeam = {
+                "name" : "green",
+                "people" : getTeam("green"),
+                "hasFocus" : false
+            },
+            $scope.blueTeam = {
+                "name" : "blue",
+                "people" : getTeam("blue"),
+                "hasFocus" : false
+            },
+            $scope.redTeam = {
+                "name" : "red",
+                "people" : getTeam("red"),
+                "hasFocus" : false
+            }
+        ];
+    });
+
+    function getTeam(color) {
+        var array = [];
+        for (var i = 0; i < $scope.people.length; i++) {
+            if ($scope.people[i].team == color) {
+                array.push($scope.people[i]);
+            }
+        }
+        return array;
+    }
+
+    $scope.focusTeam = function(team) {
+        for (var i = 0; i < $scope.teams.length; i++) {
+            $scope.teams[i].hasFocus = false;
+        }
+        team.hasFocus = true;
+        $scope.teamFocused = true;
+        $scope.focusedTeam = team;
+    };
+
+    $scope.unfocusTeam = function(team) {
+        team.hasFocus = false;
+        $scope.teamFocused = false;
+    };
+
+    $scope.players = [];
+    $scope.$watch('focusedTeam', function() {
+        if ($scope.teamFocused) {
+            var array = [];
+            for (var i = 0; i < $scope.focusedTeam.people.length; i++) {
+                if ($scope.focusedTeam.people[i].playing) {
+                    array.push($scope.focusedTeam.people[i])
+                }
+            }
+            $scope.players = array;
+        }
+    }, "true");
+
+    $scope.confirmTeam = function(team) {
+        $scope.teamPlaying = team;
+        $scope.teamPlaying.players = $scope.players;
+        var players = $scope.teamPlaying.players;
+        for (var i = 0; i < players.length; i++) {
+            var imageId = players[i].img.split("/")[4];
+            players[i].imgSrc = imageId + ".png";
+        }
+        $scope.readyToStart = true;
+    };
+
+
+    /*THE GAME VARS AND FUNCTIONS*/
+
+    $scope.start = function() {
+        $scope.hasStarted = true;
+        $scope.currentQuestionNum = 1;
+        $scope.currentPlayerNum = 0;
+        $scope.totalPlayers = $scope.teamPlaying.players.length;
+
         loadQuestion($scope.currentQuestionNum);
+        $scope.teamPlaying.players[$scope.currentPlayerNum].going = true;
     };
 
     function loadQuestion(num) {
@@ -39,38 +159,25 @@ app.controller("mainCTRL", function($scope, $http){
         }
     }
 
+    function setPlayer(currentPlayerNum) {
+        var nextNum = currentPlayerNum + 1;
+        if (nextNum == $scope.totalPlayers) {
+            nextNum = 0;
+        }
+        $scope.currentPlayerNum = nextNum;
+        $scope.teamPlaying.players[currentPlayerNum].going = false;
+        $scope.teamPlaying.players[$scope.currentPlayerNum].going = true;
+    }
+
+    $scope.nextQuestion = function(){
+        $scope.currentQuestionNum += 1;
+        loadQuestion($scope.currentQuestionNum);
+        setPlayer($scope.currentPlayerNum);
+    };
+
     $scope.revealAnswer = function(answer) {
         answer.reveal = true;
     };
-
-    $scope.reset = function() {
-        $scope.hasStarted = false;
-        $scope.currentQuestion = {};
-        $scope.currentQuestionNum = 1;
-    };
-    
-    function getTeam(color) {
-        var array = [];
-        for (var i = 0; i < $scope.people.length; i++) {
-            if ($scope.people[i].team == color) {
-                array.push($scope.people[i]);
-            }
-        }
-        return array;
-    }
-
-    $scope.$on('peopleLoaded', function(){
-        $scope.purpleTeam = getTeam("purple");
-        $scope.platinumTeam = getTeam("platinum");
-        $scope.goldTeam = getTeam("gold");
-        $scope.paisleyTeam = getTeam("paisley");
-        $scope.titaniumTeam = getTeam("titanium");
-        $scope.blackTeam = getTeam("black");
-        $scope.psychedelicTeam = getTeam("psychedelic");
-        $scope.greenTeam = getTeam("green");
-        $scope.blueTeam = getTeam("blue");
-        $scope.redTeam = getTeam("red");
-});
 
 
 });
